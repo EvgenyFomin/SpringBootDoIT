8c45bf248f690e96c2235adf2c64e7cf5a5bcdf7
EvgenyFomin <sevenup96@mail.ru>
2018-10-05 20:52:41 +0300

    fixUserService

--- src/main/java/ru/study/services/UserService.java
+++ src/main/java/ru/study/services/UserService.java
@@ -1,11 +1,9 @@
 package ru.study.services;
 
-import org.springframework.stereotype.Component;
 import ru.study.model.User;
 
 import java.util.List;
 
-@Component
 public interface UserService {
     List<User> findAll();
 
